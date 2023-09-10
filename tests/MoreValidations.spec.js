947896bb9d86162d2aa3e454565855b131d142fb
vijay <vijaysalian94@gmail.com>
2023-09-10 22:26:34 +1000

    minor updates

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -1,10 +1,9 @@
-   const {test,expect} = require('@playwright/test')
+const { test, expect } = require('@playwright/test')
 
 //test.describe.configure({mode:'parallel'});
 //test.describe.configure({mode:'serial'});
 
-test("@Web Popup validations",async({page})=>
-{
+test("@Web Popup validations", async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
 
     // await page.goto("http://google.com");
@@ -13,32 +12,30 @@ test("@Web Popup validations",async({page})=>
     await expect(page.locator("#displayed-text")).toBeVisible();
     await page.locator("#hide-textbox").click();
     await expect(page.locator("#displayed-text")).toBeHidden();
-   // await page.pause();
-    page.on('dialog',dialog => dialog.accept());
+    // await page.pause();
+    page.on('dialog', dialog => dialog.accept());
     await page.locator("#confirmbtn").click();
     await page.locator("#mousehover").hover();
     const framesPage = page.frameLocator("#courses-iframe");
     await framesPage.locator("li a[href*='lifetime-access']:visible").click();
-     const textCheck =await framesPage.locator(".text h2").textContent();
+    const textCheck = await framesPage.locator(".text h2").textContent();
     console.log(textCheck.split(" ")[1]);
 
 
 })
 
-test("Screenshot & Visual comparision",async({page})=>
-{
+test("Screenshot & Visual comparision", async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
     await expect(page.locator("#displayed-text")).toBeVisible();
-    await page.locator('#displayed-text').screenshot({path:'partialScreenshot.png'});
+    await page.locator('#displayed-text').screenshot({ path: 'partialScreenshot.png' });
     await page.locator("#hide-textbox").click();
-    await page.screenshot({path: 'screenshot.png'});
+    await page.screenshot({ path: 'screenshot.png' });
     await expect(page.locator("#displayed-text")).toBeHidden();
 });
 //screenshot -store -> screenshot -> 
-test('visual',async({page})=>
-{
+test('visual', async ({ page }) => {
     //make payment -when you 0 balance
-      await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     expect(await page.screenshot()).toMatchSnapshot('landing.png');
 
 })
--- utils/APiUtils.js
+++ utils/APiUtils.js
@@ -1,49 +1,43 @@
-class APiUtils
-{
+class APiUtils {
 
-    constructor(apiContext,loginPayLoad)
-    {
-        this.apiContext =apiContext; 
+    constructor(apiContext, loginPayLoad) {
+        this.apiContext = apiContext;
         this.loginPayLoad = loginPayLoad;
-        
+
     }
 
-    async getToken()
-     {
-        const loginResponse =  await  this.apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
-        {
-            data:this.loginPayLoad
-         } )//200,201,
+    async getToken() {
+        const loginResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
+            {
+                data: this.loginPayLoad
+            })//200,201,
         const loginResponseJson = await loginResponse.json();
-        const token =loginResponseJson.token;
+        const token = loginResponseJson.token;
         console.log(token);
         return token;
-
     }
 
-    async createOrder(orderPayLoad)
-    {
+    async createOrder(orderPayLoad) {
         let response = {};
-       response.token = await this.getToken();
-    const orderResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
-   {
-    data : orderPayLoad,
-    headers:{
-                'Authorization' :response.token,
-                'Content-Type'  : 'application/json'
-            },
+        response.token = await this.getToken();
+        const orderResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
+            {
+                data: orderPayLoad,
+                headers: {
+                    'Authorization': response.token,
+                    'Content-Type': 'application/json'
+                },
 
-   })
-   const orderResponseJson =await orderResponse.json();
-   console.log(orderResponseJson);
-  const orderId = orderResponseJson.orders[0];
-   response.orderId = orderId;
+            })
+        const orderResponseJson = await orderResponse.json();
+        console.log(orderResponseJson);
+        const orderId = orderResponseJson.orders[0];
+        response.orderId = orderId;
 
-   return response;
-}
-
+        return response;
     }
-module.exports = {APiUtils};
+
+}
 
 
 
