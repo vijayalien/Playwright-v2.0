00183658c1a94ac78c2a823454d4263cd670bdca
vijay <vijaysalian94@gmail.com>
2023-10-31 21:56:32 +1100

    js array

---
+++ tests/Javascript/array.js
@@ -0,0 +1,23 @@
+let myArray=[]
+let numbers = [1,2,3,4,5,6]
+let fruits=["apple","banana","kiwi"]
+
+//Push
+fruits.push("Avocado","test")
+console.log(fruits)
+
+
+//pop
+numbers.pop()
+console.log(numbers)
+
+//shift
+let val=numbers.shift()
+console.log(val)
+console.log(numbers)
+
+
+//unshift
+let test=fruits.unshift("melon")
+console.log(test)
+console.log(fruits)
